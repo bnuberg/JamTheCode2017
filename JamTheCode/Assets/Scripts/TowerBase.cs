4b7bbc3e90757cfdc9bb84c0fff0db83872e5ea1
Marc Gomersbach <marc.gomersbach@hva.nl>
2017-01-20 20:22:54 +0100

    Created Tower Branch with basic Tower and TowerBase script.

---
+++ JamTheCode/Assets/Scripts/TowerBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour {
+	public TowerBase parent;
+	public Tower[] children;
+
+	// Use this for initialization
+	void Start () {
+		children = new Tower[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetParent(TowerBase tower) {
+		parent = tower;
+	}
+
+	public void AddChild(Tower child) {
+		if (children.Length == 4) {
+			Debug.Log("Tower exceeds children limit!");
+		}
+		children[children.Length] = child;
+	}
+}
