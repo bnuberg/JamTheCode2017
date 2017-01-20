7aef3a7f5bab079b0154d0b0c5a14ad7dace2a37
Brian Nuberg <bnuberg@live.nl>
2017-01-20 22:40:29 +0100

    Input code

---
+++ JamTheCode/Assets/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       InputHandler();
+
+    }
+
+    private void InputHandler()
+    {
+        if (Input.GetButtonDown("X")) { Debug.Log("0"); }
+        if (Input.GetButtonDown("Circle")) { Debug.Log("1"); }
+        if (Input.GetButtonDown("Square")) { Debug.Log("2"); }
+        if (Input.GetButtonDown("Triangle")) { Debug.Log("3"); }
+    }
+}
